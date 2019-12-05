dce16389ae5eb05616f7368bfd61adb60739b513
Max Hvesser-Lewis <maxhvess@mbp-czjgh6.schibsted.no>
2019-12-05 13:05:38 +0100

    Added granite module and added this as a dependency in the sample app.

---
+++ granite/src/main/java/no/finn/granite/ui/GraniteImageGallery.kt
@@ -0,0 +1,12 @@
+package no.finn.granite.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class GraniteImageGallery
+constructor(context: Context, attrs: AttributeSet?) : FrameLayout(context, attrs) {
+
+    
+
+}
