25750e8757989deb52bfb39949f17364b9ec391a
Max Hvesser-Lewis <maxhvess@mbp-czjgh6.schibsted.no>
2019-12-05 13:17:19 +0100

    Added a very basic layout implementation.

--- granite/src/main/java/no/finn/granite/ui/GraniteImageGallery.kt
+++ granite/src/main/java/no/finn/granite/ui/GraniteImageGallery.kt
@@ -2,11 +2,15 @@ package no.finn.granite.ui
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.widget.FrameLayout
+import no.finn.granite.R
 
 class GraniteImageGallery
 constructor(context: Context, attrs: AttributeSet?) : FrameLayout(context, attrs) {
 
-    
+    init {
+        LayoutInflater.from(context).inflate(R.layout.layout_gallery_image_granite, this)
+    }
 
 }
